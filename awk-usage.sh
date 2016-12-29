a72f5457efdb8422b9a761e52164ce463c6a8c75
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2016-12-29 13:42:28 -0700

    Update awk-usage.sh

--- awk-usage.sh
+++ awk-usage.sh
@@ -2,3 +2,9 @@
 # Usage: sh awk-usage.sh
 
 awk 'BEGIN{print "Id,Name,Salary"} {print;}' OFS=',' ./input-file > ./employee.csv
+
+# Print 3rd column(i.e Salary) from employee
+awk 'BEGIN{FS=","} {print $3}' ./employee.csv
+
+# Print 3rd column(i.e Salary), calculate & print sum of salary
+awk 'BEGIN{FS=","} { sum+=$3; print $3} END{ print "Total Salary:",sum}' ./employee.csv

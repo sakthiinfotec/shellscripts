2d2af6750598b53c1b7df4e308ee9b104a3e0aee
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2016-12-29 13:10:34 -0700

    Update sed-usage.sh

--- sed-usage.sh
+++ sed-usage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Usage: sh sed-usage.sh
 
-# 1. Print comma(,) separated list of item one-by-one in a new line
+# 1. Print comma(,) separated list of items one-by-one in a new line
 STATES=Arizona,California,Texas
 
 for state in $(echo $STATES | sed "s/,/ /g"); do

7bac146560ebe8da2187a4367529d80997e6385a
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2016-12-29 13:52:01 -0700

    Update awk-usage.sh

--- awk-usage.sh
+++ awk-usage.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # Usage: sh awk-usage.sh
 
-awk 'BEGIN{print "Id,Name,Salary"} {print;}' OFS=',' ./input-file > ./employee.csv
+# Read input file with fields delimitted by space and output with fields delimitted by comma(,); Default FS is space.
+# The 'print' statement in BEGIN{} part, additionally insert column header as a first line in the output.
+awk 'BEGIN{FS=" "; OFS=","; print "Id,Name,Salary"} {print $1,$2,$3;}' ./input-file > ./employee.csv
 
 # Print 3rd column(i.e Salary) from employee
-awk 'BEGIN{FS=","} {print $3}' ./employee.csv
+awk 'BEGIN{FS=","} {print $3}' employee.csv
 
 # Print 3rd column(i.e Salary), calculate & print sum of salary
-awk 'BEGIN{FS=","} { sum+=$3; print $3} END{ print "Total Salary:",sum}' ./employee.csv
+awk 'BEGIN{FS=","} { sum+=$3; print $3} END{ print "Total Salary:",sum}' employee.csv

07d789df127623f178ab95c8c46d4a66fbba351c
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2016-12-29 13:29:14 -0700

    Update sed-usage.sh

--- sed-usage.sh
+++ sed-usage.sh
@@ -7,3 +7,18 @@ STATES=Arizona,California,Texas
 for state in $(echo $STATES | sed "s/,/ /g"); do
   echo $state
 done
+
+# 2. Insert a line or column header at first row in a file
+sed -i '1s/^/Id,Name,Salary\n/' employee-list.csv
+
+# 3. Delete line(s) in the input file
+
+# Delete 2nd line from the input file
+sed 2d ./input-file
+
+# To remove the line and print the output to standard out:
+sed '/regex pattern to match/d' ./input-file
+sed '/regex pattern to match/d' ./input-file > ./output-file
+
+# To directly modify the file (and create a backup called, for eg. input-file.bak):
+sed -i.bak '/pattern to match/d' ./input-file

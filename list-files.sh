f2cf57c713c3ab567fc7108387b92067a9a59d4f
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2016-12-29 13:35:43 -0700

    Update list-files.sh

--- list-files.sh
+++ list-files.sh
@@ -14,15 +14,3 @@ do
   fi
 done
 
-# To print file name
-filename=`basename $0`
-echo $filename
-
-# To print directory name of a given file path
-dir=$(dirname "/home/sakthi/wordcount/wordcount.py")
-echo "Dir: $dir"
-
-# To print directory name of a current script
-filename=`dirname $0`
-cd $filename
-echo "PWD: $(pwd)"

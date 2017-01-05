1935f840c1b46aafbf02fa56791be26e1c4d6fe8
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2017-01-05 11:50:25 -0700

    Update base-path.sh

--- base-path.sh
+++ base-path.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Usage: sh base-path.sh
 
-# To print file name
+# To print file/script name(with extension if supplied)
 filename=`basename $0`
 echo $filename
 
-# To print directory name of a given file path
+# To print directory name of a given file path. Below command output will be /home/sakthi/wordcount.
 dir=$(dirname "/home/sakthi/wordcount/wordcount.py")
 echo "Dir: $dir"
 
@@ -13,3 +13,6 @@ echo "Dir: $dir"
 filename=`dirname $0`
 cd $filename
 echo "PWD: $(pwd)"
+
+# or the above same command in a single line
+echo "$( cd $( dirname $0 ) && pwd )"

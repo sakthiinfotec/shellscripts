3bde90d092e3a57e2de4ee510182a57268666905
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2017-01-19 11:28:39 -0700

    Create common.sh

---
+++ common.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Commonly used commands
+# Usage: sh common.sh
+
+# Find disk usage
+du -h --summarize * | sort -h
+
+scp /source/file/path user@target-host:/path
+
+# use -r if folder
+scp -r /source/folder/path user@target-host:/path
+
+# To list down the processes which are using files right now
+lsof -r1 /tmp/sort9nkNt9
+lsof -r1 /some/dir/*
+
+# This will show processes accessing /some/dir and refresh every 2 seconds.
+lsof +r2 | grep '/tmp/sort' 
+
+# We can use ls and grep to find out the files used by chrome
+ls -l /proc/*/fd | grep "chrome"
+
+# List a sorted uniq column values
+cat /tmp/tmp-file | awk -F: '{print $2}' | sort | uniq

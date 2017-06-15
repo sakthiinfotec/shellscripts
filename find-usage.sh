58e83ce7d9bc11c753ce75018d033f087798dd36
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2017-06-15 10:48:54 -0700

    Create find-usage.sh

--- find-usage.sh
+++ find-usage.sh
@@ -7,3 +7,7 @@ find ./lib ! -name "*.jar"
 
 find . -type f -exec dos2unix {} \;
 find . -type f -print0 | xargs -0 -n 1 -P 4 dos2unix 
+
+# List files by size
+find -maxdepth 1 -name "log_file.log" -size -100k
+find -maxdepth 1 -name "log_file.log" -size -100k -exec script \;
